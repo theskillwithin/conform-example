d0bfc1c4f2dc8e6f2b7836a927a1813e8568c312
Austin Peterson <austinppeterson@gmail.com>
2025-12-18 21:22:06 -0700

    Add react-router-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [
+      reactRouterDevTools(),
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths(),
+    ],
     server: {
       port: 5173,
       host: true,
