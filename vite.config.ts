e17b87d35159a01fcffc6588433ed84e61a5fddc
Austin Peterson <austinppeterson@gmail.com>
2025-12-18 21:25:40 -0700

    react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const ReactCompilerConfig = {};
 
 export default defineConfig(() => {
   return {
@@ -11,6 +14,13 @@ export default defineConfig(() => {
       tailwindcss(),
       reactRouter(),
       tsconfigPaths(),
+      babel({
+        filter: /\.[jt]sx?$/,
+        babelConfig: {
+          presets: ["@babel/preset-typescript"],
+          plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+        },
+      }),
     ],
     server: {
       port: 5173,
