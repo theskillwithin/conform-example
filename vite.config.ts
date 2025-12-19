bc93ca55ba963289b401ac74a78866474006731d
Austin Peterson <austinppeterson@gmail.com>
2025-12-18 21:26:46 -0700

    comment out react-router-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { reactRouterDevTools } from "react-router-devtools";
+// import { reactRouterDevTools } from "react-router-devtools";
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -10,7 +10,7 @@ const ReactCompilerConfig = {};
 export default defineConfig(() => {
   return {
     plugins: [
-      reactRouterDevTools(),
+      // reactRouterDevTools(),
       tailwindcss(),
       reactRouter(),
       tsconfigPaths(),
